8bff4d2fae69b3186f88806bf7b2c06aaec316c3
Lenovo <Lenovo@LENOVO-T420>
2017-08-28 12:24:14 +0200

    he puesto hola

--- src/main/java/com/lab/persistence/mapper/TestMapper.java
+++ src/main/java/com/lab/persistence/mapper/TestMapper.java
@@ -3,7 +3,7 @@ package com.lab.persistence.mapper;
 import com.lab.persistence.model.TestModel;
 import java.util.List;
 
-
+//aloha
 public interface TestMapper
 {
   public List<TestModel> testMapper(TestModel obj) throws Exception;
